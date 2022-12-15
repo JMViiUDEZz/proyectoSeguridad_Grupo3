80487a42448105fc86976c3101f5d4cd05fedc33
José María Viúdez <jmviudezp2003@gmail.com>
2022-12-16 00:41:21 +0100

    Merge con master

---
+++ differentialBackup.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+DHOME="/home/jose"
+DBASE="$DHOME/backup"
+DDATA="$DHOME/datos"
+DATE="`date +%d%b%y`"
+DSNAP="$DBASE/snapshot"
+PREFIJO="differential"
+DDEST="$DBASE/$PREFIJO"
+NOMBRE=`echo "$DDATA" | egrep -o "[^/]+$"`
+SNAP="$DSNAP"/"$PREFIJO"_"$NOMBRE"_"$DATE".snap
+TAR="$DDEST"/"$PREFIJO"_"$NOMBRE"_"$DATE".tgz
+TARFULL=`ls -l "$DDEST" | grep full_"$NOMBRE" | awk '{printf $NF}'`
+FECHA=`date -r "$DDEST"/"$TARFULL" "+%F %H:%M"`
+
+for dir in "$DBASE" "$DSNAP" "$DDEST"
+do
+    mkdir -p "$dir"
+    if [ "$?" -ne "0" ]
+    then
+        case "$dir" in
+            "$DBASE") echo "ERROR: No se pudo crear el directorio $DBASE.\n";;
+            "$DSNAP") echo "ERROR: No se pudo crear el directorio $DSNAP.\n";;
+            "$DDEST") echo "ERROR: No se pudo crear el directorio $DDEST.\n";;
+        esac
+    fi
+done
+
+echo "\nBackup para $DDATA será $PREFIJO\n"
+                      
+if [ ! -d "$DDATA" ]
+then
+        echo "ERROR: No es posible respaldar $DDATA. El directorio no existe\n"
+        return
+fi        
+
+if [ -f "$SNAP" ]
+then
+        rm "$SNAP"
+        echo "Archivo histórico $SNAP eliminado\n"
+fi
+
+if [ ! -e "$TAR" ]
+then
+        rm "$TAR"
+        echo "Archivo Backup $TAR eliminado\n"
+fi
+
+echo "Iniciando $PREFIJO backup para $DDATA\t $(date "+[%x %X]")\n"
+cd "$DDATA"
+tar -cpvzf "$TAR" * -N "$FECHA"
+
+if [ -e "$TAR" ]
+then
+    BACKUP="\n$PREFIJO backup ha sido creada correctamente\n"
+else
+    BACKUP="\nERROR: $PREFIJO backup no ha sido creada correctamente\n"
+fi
+
+echo "$BACKUP"
